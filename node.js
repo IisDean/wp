71c28d4c080882845f90bc6abb062185fccbf75c
Liu min <781775946@qq.com>
2018-10-18 18:08:38 +0800

    1

--- node.js
+++ node.js
@@ -27,19 +27,54 @@ loginRouter.use('/res',(req,res)=>{
 		}else{
 			conn.query('SELECT user FROM `user` WHERE user="'+ req.query.user +'";',(err,data)=>{
 				if( data.length > 0 ){
-					res.sned({'ok': 0,'msg': '用户名已占用'});
+					res.send({'ok': 0,'msg': '用户名已占用'});
 					conn.end();
 				}else{
 					conn.query('INSERT INTO `user` (`user`,`password`) VALUE("'+ req.query.user +'","'+ req.query.pass +'");',(err,data)=>{
 						if(err){
 							console.log(err);
 							res.send({'ok': 0,'msg': '数据库连接失败'});
+							conn.end();
 						}else{
 							res.send({'ok': 1,'msg': '恭喜您,注册成功!'});
+							conn.query(`CREATE TABLE ${req.query.user}
+									(
+										ID int(255) NOT NULL AUTO_INCREMENT,
+										LastName varchar(255) NOT NULL,
+										hashName varchar(255) NOT NULL,
+										lastTime varchar(255) NOT NULL,
+										type varchar(255),
+									)
+								`);
 						}
-						conn.end();
 					});
 				}
+			});
+		}
+	});
+});
+
+loginRouter.use('/login',(req,res)=>{
+	// console.log(req.query);
+	var Pool = mysql.createPool({
+		'host': 'localhost',
+		'user': 'root',
+		'password': 'root',
+		'database': 'wp'
+	});
+	Pool.getConnection((err,conn)=>{
+		if( err ){
+			console.log(err);
+			res.send({'ok': 0,'msg': '数据库连接失败'});
+			conn.end();		
+		}else{
+			conn.query('SELECT `user`,`password` FROM user WHERE user="'+ req.query.user +'";',(err,data)=>{
+				if( data.length > 0 && req.query.pass == data[0].password ){
+					res.send({'ok': 1,'msg': '欢迎回来！'});
+				}else{
+					res.send({'ok': 0,'msg': '用户名或密码错误！'});
+				}
+				conn.end();
 			});
 		}
 	});
