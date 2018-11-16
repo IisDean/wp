f88b8a5742a732a3ab16594dc8cd98580c6f653a
Liu min <781775946@qq.com>
2018-11-16 16:27:30 +0800

    ->文件上传->删除

--- js/index.js
+++ js/index.js
@@ -1,79 +1,114 @@
 
-    //this js for dean
-    (()=>{
+//this js for dean
+(()=>{
 
-        //显示登录/注册弹窗
-        $(".J-login-show").on("click",function(){
-            // $('.J-user-node').val('');
-            // $(".J-pass-node").val('');
-            $("form-wrap .form-group").removeClass('has-error');
+    //显示登录/注册弹窗
+    $(".J-login-show").on("click",function(){
+        // $('.J-user-node').val('');
+        // $(".J-pass-node").val('');
+        $("form-wrap .form-group").removeClass('has-error');
+    });
+
+    //注册
+    $(".J-res-btn").on({
+        click(){
+            $.ajax({
+                url: 'http://localhost:8020/login/res',
+                type: 'get',
+                data: {
+                    'user': $('.J-user-node').val(),
+                    'pass': $(".J-pass-node").val(),
+                },
+                success: function(data){
+                    if(data.ok == 1){
+                        console.log(data.msg);
+                        $("form-wrap .form-group").removeClass('has-error');
+                    }else{
+                        console.log(data.msg);
+                        $("form-wrap .form-group").addClass('has-error');
+                    }
+                },
+            });
+        }
+    });
+
+    //登录
+    $(".J-login-btn").on({
+        click(){
+    		var user = $('.J-user-node').val();
+    		var pass = $(".J-pass-node").val();
+            $.ajax({
+                url: 'http://localhost:8020/login/login',
+                type: 'get',
+                data: {
+                    'user': user,
+                    'pass': pass,
+                },
+                success: function(data){
+                    if(data.ok == 1){
+                        console.log(data.msg);
+                        $(".myModal1").modal('hide');
+                        $("form-wrap .form-group").removeClass('has-error');
+                        $(".file-container").removeClass('hide');
+                        $("#userName").val(user);
+                        createFileList(data.data);
+                    }else{
+                        console.log(data.msg);
+                        $("form-wrap .form-group").addClass('has-error');
+                    }
+                },
+            });
+        }
+    });
+
+    //文件上传
+    $(".J-upload-btn").on("click",function(){
+        var oForm = new FormData();
+        oForm.append('files', fileNode.files[0]);
+        var userName = $('.J-user-node').val();
+        oForm.append('userName', userName);
+        if( window.XMLHttpRequest ){
+            var xhr = new XMLHttpRequest();
+        }else{
+            var xhr = new ActiveXObject('Microsoft.XMPHTTP');
+        }
+        xhr.open('post','http://localhost:8020/file/upload', true);
+        xhr.send(oForm);
+        xhr.onload = ()=>{
+            var data = JSON.parse(xhr.responseText);
+            createFileList([data]);
+        }
+    });
+
+    //文件列表添加记录
+    function createFileList(data){
+    	var str = '';
+    	var num = $("#fileWrap tr").length;
+    	data.forEach(function(ev,index){
+    		str += '<tr><td>'+ (num + index + 1) +'</td><td>'+ ev.LastName +'</td><td>'+ ev.size +'</td><td>'+ ev.lastTime +'</td><td>0</td><td><button data-hash="'+ ev.hashName +'" class="btn btn-default btn-danger btn-xs J-delete-btn">删除</button></td></tr>';
+    	});
+    	$("#fileWrap").append(str);
+    }
+
+    //删除文件
+    $("#fileWrap").on("click",'.J-delete-btn', function(){
+    	var hashName = $(this).attr('data-hash');
+    	console.log(hashName);
+        $.ajax({
+            url: 'http://localhost:8020/file/delete',
+            type: 'get',
+            data: {
+            	'userName': $('.J-user-node').val(),
+                'hashName': hashName,
+            },
+            success: function(data){
+                if(data.ok == 1){
+                    console.log(data.msg);
+                }else{
+                    console.log(data.msg);
+                }
+            },
         });
+    });
 
-        //注册
-        $(".J-res-btn").on({
-            click(){
-                $.ajax({
-                    url: 'http://localhost:8020/login/res',
-                    type: 'get',
-                    data: {
-                        'user': $('.J-user-node').val(),
-                        'pass': $(".J-pass-node").val(),
-                    },
-                    success: function(data){
-                        if(data.ok == 1){
-                            console.log(data.msg);
-                            $("form-wrap .form-group").removeClass('has-error');
-                        }else{
-                            console.log(data.msg);
-                            $("form-wrap .form-group").addClass('has-error');
-                        }
-                    },
-                });
-            }
-        });
-
-        //登录
-        $(".J-login-btn").on({
-            click(){
-                $.ajax({
-                    url: 'http://localhost:8020/login/login',
-                    type: 'get',
-                    data: {
-                        'user': $('.J-user-node').val(),
-                        'pass': $(".J-pass-node").val(),
-                    },
-                    success: function(data){
-                        if(data.ok == 1){
-                            console.log(data.msg);
-                            $(".myModal1").modal('hide');
-                            $("form-wrap .form-group").removeClass('has-error');
-                            $(".file-container").removeClass('hide');
-                            $("#userName").val($('.J-user-node').val());
-                        }else{
-                            console.log(data.msg);
-                            $("form-wrap .form-group").addClass('has-error');
-                        }
-                    },
-                });
-            }
-        });
-
-        //文件上传
-        $(".J-upload-btn").on("click",function(){
-            var oForm = new FormData();
-            oForm.append('files', fileNode.files[0]);
-            var userName = $('.J-user-node').val();
-            oForm.append('userName', userName);
-            if( window.XMLHttpRequest ){
-                var xhr = new XMLHttpRequest();
-            }else{
-                var xhr = new ActiveXObject('Microsoft.XMPHTTP');
-            }
-            xhr.open('post','http://localhost:8020/file/upload', true);
-            xhr.send(oForm);
-            xhr.onload = ()=>{
-                console.log(xhr.responseText);
-            }
-        });
-
-    })();
+})();
--- node.js
+++ node.js
@@ -15,8 +15,11 @@ server.use('/file',fileRouter);
 server.use('/login',loginRouter);
 
 fileRouter.use('/upload',(req,res)=>{
-	console.log(req.files);
-	var newName = path.parse(req.files[0].originalname).ext
+	var newName = path.parse(req.files[0].originalname).ext;
+	var hashName = req.files[0].filename + path.parse(req.files[0].originalname).ext;
+	var lastTime = new Date().toLocaleString();
+	var size = (req.files[0].size/1024/1024).toFixed(2) +'M';
+	console.log(size);
 	fs.rename(req.files[0].path,req.files[0].path+newName,(err)=>{
 		if( err ){
 			res.send(err);
@@ -28,18 +31,57 @@ fileRouter.use('/upload',(req,res)=>{
 				'database': 'wp'
 			});
 			Pool.getConnection((err,conn)=>{
+				console.log(req.body.userName);
 				if( err ){
+					console.log(err);
 					res.send({'status': 0,'msg': '数据库连接失败'});
 					conn.end();
 				}else{
-					conn.query('INSERT INTO'+ req.files[0].)
+					conn.query('INSERT INTO `'+ req.body.userName +'` (`LastName`,`hashName`,`lastTime`,`size`,`download`) VALUES("'+ req.files[0].originalname +'","'+ hashName +'","'+ lastTime +'","'+ size+'","0");', (err)=>{
+						if( err ){
+							console.log(err);
+							res.send({status: 0,msg: '数据库连接失败'});
+						}else{
+							res.send({status: 1,msg: '上传成功！',lastName: req.files[0].originalname,hashName: hashName,lastTime: lastTime,size: size});
+						}	
+						conn.end();				
+					});				
 				}
 			});
-			res.send({status: 1,msg: '上传成功！'});
 		}
 	});
 });
 
+//删除
+fileRouter.use('/delete',(req,res)=>{
+	console.log(req.query.hashName);
+	var Pool = mysql.createPool({
+		'host': 'localhost',
+		'user': 'root',
+		'password': 'root',
+		'database': 'wp'
+	});
+	Pool.getConnection((err,conn)=>{
+		if( err ){
+			cosole.log(err);
+			res.send({'status': 0, msg: '数据库连接失败'});
+			conn.end();
+		}else {
+			conn.query('DELETE FROM `'+ req.query.userName +'` WHERE `hashName`="'+ req.query.hashName +'";',(err)=>{
+				if( err ){
+					console.log(err);
+					res.send({'status': 0, msg: '数据库连接失败'});
+					conn.end();
+				}else{
+					res.send({'status': 1, msg: '删除成功'});
+					conn.end();
+				}
+			});
+		}
+	});
+});
+
+//注册
 loginRouter.use('/res',(req,res)=>{
 	// console.log(req.query);
 	var Pool = mysql.createPool({
@@ -90,6 +132,7 @@ loginRouter.use('/res',(req,res)=>{
 	});
 });
 
+//登录
 loginRouter.use('/login',(req,res)=>{
 	// console.log(req.query);
 	var Pool = mysql.createPool({
@@ -106,11 +149,18 @@ loginRouter.use('/login',(req,res)=>{
 		}else{
 			conn.query('SELECT `user`,`password` FROM user WHERE user="'+ req.query.user +'";',(err,data)=>{
 				if( data.length > 0 && req.query.pass == data[0].password ){
-					res.send({'ok': 1,'msg': '欢迎回来！'});
+					conn.query('SELECT `LastName`,`lastTime`,`size`,`hashName` FROM `'+ req.query.user +'`', (err,data)=>{
+						if( err ){
+							console.log(err);
+							res.send({'ok': 0,'msg': '数据查询失败'});
+						}else {
+							res.send({'ok': 1,'msg': '欢迎回来！', data: data});
+						}
+					});
 				}else{
 					res.send({'ok': 0,'msg': '用户名或密码错误！'});
+					conn.end();
 				}
-				conn.end();
 			});
 		}
 	});
